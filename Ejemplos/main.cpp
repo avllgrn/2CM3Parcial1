96868aa084a8d16c7bfc907c239c7e96ee7e9bd9
Alfredo Villagran Olguin <avillagran@ipn.mx>
2024-02-27 09:26:35 -0600

    Suma de puntos (con variables)

--- Ejemplos/main.cpp
+++ Ejemplos/main.cpp
@@ -2,66 +2,23 @@
 using namespace std;
 
 int main(void){
-    cout << "Hello world!" << endl;
-    return 0;
-}
-#include <iostream>
-#include <stdlib.h>
-#include <string.h>
-using namespace std;
+    float x1,y1,x2,y2,x3,y3;
 
-void funcion(void){
-};
+    cout << "Ingresa x1 ";
+    cin >> x1;
+    cout << "Ingresa y1 ";
+    cin >> y1;
+    cout << "Ingresa x2 ";
+    cin >> x2;
+    cout << "Ingresa y2 ";
+    cin >> y2;
 
-class Persona{
-public:
-    int anioNacimiento;
-    float estatura;
-    string Nombre;
-    char CURP[16];
-    Persona(void){
-        cout<<"Construido por Persona(void)"<<endl;
-        anioNacimiento = 2005;
-        CURP[0] = '\0';
-    };
-    Persona(int aN, char c[16]){
-        cout<<"Construido por Persona(int aN, char c[16])"<<endl;
-        anioNacimiento = aN;
-        strcpy(CURP,c);
-    };
-    ~Persona(){
-        cout<<"Objeto destruido..."<<endl;
-    };
-    int dimeTuEdad(void){
-        return 2024 - anioNacimiento;
-    };
-    string dimeTuNombre(void){
-        return Nombre;
-    };
-};
+    x3 = x1+x2;
+    y3 = y1+y2;
 
-int main(void){
-    Persona Juan,Pedro;
-    Persona Rosa(2000,"ELCURPDEROSA");
-    Persona Maria(2003,"ELCURPDEMARIA");
-
-    system("pause");
-    system("cls");
-
-    cout << "Juan.anioNacimiento: " << Juan.anioNacimiento << endl;
-    cout << "Juan.CURP: " << Juan.CURP << endl<< endl;
-
-    cout << "Pedro.anioNacimiento: " << Pedro.anioNacimiento << endl;
-    cout << "Pedro.CURP: " << Pedro.CURP << endl<< endl;
-
-    cout << "Rosa.anioNacimiento: " << Rosa.anioNacimiento << endl;
-    cout << "Rosa.CURP: " << Rosa.CURP << endl<< endl;
-
-    cout << "Maria.anioNacimiento: " << Maria.anioNacimiento << endl;
-    cout << "Maria.CURP: " << Maria.CURP << endl<< endl;
-
-    cout<<"Fin del programa"<<endl<<endl;
-    system("pause");
+    cout << "P1(" << x1 << "," << y1 << ")" << endl;
+    cout << "P2(" << x2 << "," << y2 << ")" << endl;
+    cout << "P3(" << x3 << "," << y3 << ")" << endl;
 
     return 0;
 }
