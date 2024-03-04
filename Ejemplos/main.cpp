52eeb982989f43676793891025c7d3ae0eae87dd
Alfredo Villagran Olguin <avillagran@ipn.mx>
2024-03-03 18:35:47 -0600

    Clase Punto2D y programa que suma dos instancias de ella

--- Ejemplos/main.cpp
+++ Ejemplos/main.cpp
@@ -1,24 +1,76 @@
 #include <iostream>
-#include <math.h>
+#include <stdlib.h>
 using namespace std;
 
+class Punto2D{
+private:
+    double x;
+    double y;
+public:
+    Punto2D(void){
+        Punto2D::x = 0.0;
+        Punto2D::y = 0.0;
+        cout<<"Objeto construido por Punto2D(void)"<<endl;
+    };
+    Punto2D(double x, double y){
+        Punto2D::x = x;
+        Punto2D::y = y;
+        cout<<"Objeto construido por Punto2D(double x, double y)"<<endl;
+    };
+    ~Punto2D(void){
+        cout<<"Objeto destruido =)"<<endl;
+    };
+    void pideleAlUsuarioTuEstado(void){
+        cout << "Dame mi x ";
+        cin >> this->x;
+        cout << "Dame mi y ";
+        cin >> this->y;
+    };
+    void muestraTuEstado(void){
+        cout << "(" << this->x << "," << this->y << ")";
+    };
+    double dameTuAtributoX(void){
+        return this->x;
+    };
+    void modificaTuAtributoX(double x){
+        this->x = x;
+    };
+    double dameTuAtributoY(void){
+        return this->y;
+    };
+    void modificaTuAtributoY(double y){
+        this->y = y;
+    };
+};
+
 int main(void){
-    float x1,y1,x2,y2,d;
+    cout << endl << "Inicia main" << endl << endl;
+    Punto2D P1,P2,P3;
+    system("pause");
+    system("cls");
 
-    cout << "Ingresa x1 ";
-    cin >> x1;
-    cout << "Ingresa y1 ";
-    cin >> y1;
-    cout << "Ingresa x2 ";
-    cin >> x2;
-    cout << "Ingresa y2 ";
-    cin >> y2;
+    cout << endl;
+    cout << "Ingresa P1" << endl;
+    P1.pideleAlUsuarioTuEstado();
+    cout << endl;
+    cout << "Ingresa P2" << endl;
+    P2.pideleAlUsuarioTuEstado();
 
-    d = sqrt(pow(y2-y1,2) + pow(x2-x1,2));
+    P3.modificaTuAtributoX( P1.dameTuAtributoX() + P2.dameTuAtributoX() );
+    P3.modificaTuAtributoY( P1.dameTuAtributoY() + P2.dameTuAtributoY() );
 
-    cout << "P1(" << x1 << "," << y1 << ")" << endl;
-    cout << "P2(" << x2 << "," << y2 << ")" << endl;
-    cout << "d = "<< d << endl;
+    cout << endl << endl;
+    cout << "  P1";
+    P1.muestraTuEstado();
+    cout << endl;
+    cout << "+ P2";
+    P2.muestraTuEstado();
+    cout << endl;
+    cout << "= P3";
+    P3.muestraTuEstado();
+    cout << endl << endl;
 
+    cout<<endl<<"Termina main"<<endl<<endl;
+    system("pause");
     return 0;
 }
