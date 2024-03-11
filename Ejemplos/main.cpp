8dcb8c9ed37b47195f4cc02b7e335a540f9b5b17
Alfredo Villagran Olguin <avillagran@ipn.mx>
2024-03-11 17:17:02 -0600

    Cuenta tipos de caracteres en un string

--- Ejemplos/main.cpp
+++ Ejemplos/main.cpp
@@ -3,21 +3,60 @@
 #include <string>
 using namespace std;
 
+bool esNumero(char c);
+bool esMayuscula(char c);
+bool esMinuscula(char c);
+bool esLetra(char c);
+bool esEspecial(char c);
+
 int main(void){
-    string Cadena1,Cadena2;
-    float numero1,numero2,numero3;
+    int i,numeros=0, minusculas=0, mayusculas=0,letras=0, especiales=0;
+    string Cadena;
 
-    cout<<"Ingresa un numero ";
-    cin>>Cadena1;
-    cout<<"Ingresa otro numero ";
-    cin>>Cadena2;
+    cout<<"Ingresa cadena ";
+    cin>>Cadena;
 
-    numero1 = atof(Cadena1.c_str());
-    numero2 = atof(Cadena2.c_str());
+    for(i=0; i<Cadena.size(); i++){
+        if(esNumero(Cadena.at(i)))
+            numeros++;
 
-    numero3 = numero1 + numero2;
+		if(esMinuscula(Cadena.at(i)))
+            minusculas++;
 
-    cout<<numero1<<" + "<<numero2<<" = "<<numero3<<endl<<endl;
+        if(esMayuscula(Cadena.at(i)))
+            mayusculas++;
+
+        if(esLetra(Cadena.at(i)))
+            letras++;
+
+        if(esEspecial(Cadena.at(i)))
+            especiales++;
+    }
+
+    cout<<endl<<endl
+        <<"tiene "<<Cadena.size()<<" caracteres."<<endl
+        <<"tiene "<<numeros <<" numeros."<<endl
+        <<"tiene "<<minusculas<<" minusculas."<<endl
+        <<"tiene "<<mayusculas<<" mayusculas."<<endl
+        <<"tiene "<<letras<<" letras."<<endl
+        <<"tiene "<<especiales<<" caracteres especiales."
+        <<endl<<endl;
 
     return 0;
+}
+
+bool esNumero(char c){
+    return 48<=c && c <=57;
+}
+bool esMayuscula(char c){
+    return 65<=c && c <=90;
+}
+bool esMinuscula(char c){
+    return 97<=c && c <=122;
+}
+bool esLetra(char c){
+    return esMayuscula(c) || esMinuscula(c);
+}
+bool esEspecial(char c){
+    return !esLetra(c) && !esNumero(c);
 }
