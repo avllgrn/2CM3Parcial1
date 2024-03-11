c0d0a75061fe3376d87f9349f7100276862f9da7
Alfredo Villagran Olguin <avillagran@ipn.mx>
2024-03-11 17:11:32 -0600

    Suma de dos floats (guardados en objetos string)

--- Ejemplos/main.cpp
+++ Ejemplos/main.cpp
@@ -4,69 +4,20 @@
 using namespace std;
 
 int main(void){
-    string Cadena1;
-    string Cadena2("de lenguaje C");
-    string Cadena3(Cadena2);
+    string Cadena1,Cadena2;
+    float numero1,numero2,numero3;
 
-    cout<<"Cadena1: "<<Cadena1<<endl;
-    cout<<"Cadena2: "<<Cadena2<<endl;
-    cout<<"Cadena3: "<<Cadena3<<endl<<endl;
-    system("pause");
-    cout<<endl<<endl;
+    cout<<"Ingresa un numero ";
+    cin>>Cadena1;
+    cout<<"Ingresa otro numero ";
+    cin>>Cadena2;
 
-    Cadena1.assign("Dos ");
-    Cadena3.assign(Cadena1);
+    numero1 = atof(Cadena1.c_str());
+    numero2 = atof(Cadena2.c_str());
 
-    cout<<"Cadena1: "<<Cadena1<<endl;
-    cout<<"Cadena2: "<<Cadena2<<endl;
-    cout<<"Cadena3: "<<Cadena3<<endl<<endl;
-    system("pause");
-    cout<<endl<<endl;
+    numero3 = numero1 + numero2;
 
-    Cadena3.append("cadenas ");
-    Cadena3.append(Cadena2);
-
-    cout<<"Cadena1: "<<Cadena1<<endl;
-    cout<<"Cadena2: "<<Cadena2<<endl;
-    cout<<"Cadena3: "<<Cadena3<<endl<<endl;
-    system("pause");
-    cout<<endl<<endl;
-
-    cout<<"Cadena1 tiene "<<Cadena1.size()<<" caracteres"<<endl;
-    cout<<"Cadena2 tiene "<<Cadena2.size()<<" caracteres"<<endl;
-    cout<<"Cadena3 tiene "<<Cadena3.size()<<" caracteres"<<endl<<endl;
-
-    cout<<"Cadena1 tiene "<<Cadena1.length()<<" caracteres"<<endl;
-    cout<<"Cadena2 tiene "<<Cadena2.length()<<" caracteres"<<endl;
-    cout<<"Cadena3 tiene "<<Cadena3.length()<<" caracteres"<<endl<<endl;
-    system("pause");
-    cout<<endl<<endl;
-    system("pause");
-    system("cls");
-
-    cout<<"Cadena1: "<<Cadena1<<endl;
-    cout<<"Cadena2: "<<Cadena2<<endl;
-    cout<<"Cadena3: "<<Cadena3<<endl<<endl;
-
-    cout<<"Cadena1.at(0): "<<Cadena1.at(0)<<endl;
-    cout<<"Cadena2.at(1): "<<Cadena2.at(10)<<endl;
-    cout<<"Cadena3.at(20): "<<Cadena3.at(20)<<endl<<endl;
-
-    int i;
-    cout<<endl<<"Cadena 1"<<endl;
-    for(i=0;i<Cadena1.size();i++){
-        cout<<"Cadena1.at("<<i<<") => "<<Cadena1.at(i)<<endl;
-    }
-
-    cout<<endl<<"Cadena 2"<<endl;
-    for(i=0;i<Cadena2.size();i++){
-        cout<<"Cadena2.at("<<i<<") => "<<Cadena2.at(i)<<endl;
-    }
-
-    cout<<endl<<"Cadena 3"<<endl;
-    for(i=0;i<Cadena3.size();i++){
-        cout<<"Cadena3.at("<<i<<") => "<<Cadena3.at(i)<<endl;
-    }
+    cout<<numero1<<" + "<<numero2<<" = "<<numero3<<endl<<endl;
 
     return 0;
 }
